8ec9123c6ef3d13727690085e09c2009d2a14a5f
Tiago Monteiro <tmonteiro@est.frba.utn.edu.ar>
2020-09-01 14:36:14 -0300

    mejorando scripts

--- 02_cuat/my_scripts/scp_transfer.sh
+++ 02_cuat/my_scripts/scp_transfer.sh
@@ -2,20 +2,73 @@
 # cartpeta del proyecto.
 
 #! /usr/bin/bash
-FILE=${1:-}
-DEF_PATH=${2:-server/}
+file=
+defPath="server/"
+ipAddr="192.168.7.2"
+usrName="ubuntu"
+pwd="temppwd"
 
-if [ -z "$FILE" ]
+
+#____________________ Functions ____________________#
+
+help(){
+    printf "\nModo de uso:\n\t./scp_transfer.sh [OPCIONES]\n"
+    printf "\nOpciones:\n\t-f: Archivo a copiar DEBE DEFINIRSE OBLIGATORIAMENTE.\n"
+    printf "\t-r: Path al cual se copia el archivo. Por defecto es ~/server/.\n"
+    printf "\t-d: Dirección ip a la cual se copia el archivo. Por defecto es 192.168.7.2.\n"
+    printf "\t-n: Nombre de usuario. Por defecto es \"ubuntu\".\n"
+    printf "\t-p: Password. Por defecto es \"temppwd\".\n"
+    printf "\t-h: Ayuda. Muestra este menu.\n\n"
+}
+
+transferFile(){
+    sshpass -p $pwd scp $file $usrName@$ipAddr:$defPath
+
+    if [ $? -eq 0 ];
+    then
+        printf "\n\t>>>> ¡Archivo copiado a la BBB en "\"$defPath\"" con éxito! <<<<\n\n"
+    else
+        printf "\n\t>>>>¡¡Error al copiar el archivo!!.<<<<\n\n"
+    fi
+}
+
+
+#____________________ Program flow ____________________#
+
+### Parameters Parser ###
+while [ "$1" != "" ]; do
+    case $1 in
+        -f )    shift
+                    file=$1
+                    ;;
+        -d )    shift
+                    ipAddr=$1
+                    ;;
+        -n )    shift
+                    usrName=$1
+                    ;;
+	    -p )    shift
+                    pwd=$1
+                    ;;
+        -r )    shift
+                    defPath=$1
+                    ;;
+        -h )        help
+                    exit
+                    ;;
+        * )         help
+                    exit 1
+    esac
+    shift
+done
+
+if [ -z "$pwd" ] || [ -z "$ipAddr" ] || [ -z "$defPath" ] || [ -z "$usrName" ] || [ -z "$file" ]
 then
-    printf "\nDebe ingresar el nombre del archivo!\nModo de uso:\n\n\t./scp_transfer.sh <ARCHIVO> <DIRECTORIO_DESTINO>\n\nDe no especificarse destino, se guarda en la carpeta ~/server.\n\n"
+    help
     exit
 fi
 
-sshpass -p temppwd scp $FILE ubuntu@192.168.7.2:$DEF_PATH
+### Transfer ###
+transferFile
 
-if [ $? -eq 0 ];
-then
-    printf "\nArchivo copiado a la BBB en "$DEF_PATH"\n\n"
-else
-    printf "\n>>>>¡¡Error al copiar el archivo!!.<<<<\n\n"
-fi
+
--- 02_cuat/my_scripts/ssh_connect.sh
+++ 02_cuat/my_scripts/ssh_connect.sh
@@ -1,7 +1,59 @@
-#Script que me conecta a la BBB y me deja en la carpeta que me pasan por 
-# argumento o en su defecto en la del proyecto "server/".
+
+#Script que me conecta a la BBB los valores por defecto son los de las
+# variables definidas abajo. Todas esas variables se pueden modificar
+# en el momento que se llama al script.
 
 #! /usr/bin/bash
-DEF_PATH=${1:-server/}
+initPath="server/"
+ipAddr="192.168.7.2"
+usrName="ubuntu"
+pwd="temppwd"
 
-sshpass -p temppwd ssh -t ubuntu@192.168.7.2 "cd $DEF_PATH ; bash"
+#____________________ Functions ____________________#
+connect(){
+    sshpass -p $pwd ssh -t $usrName@$ipAddr "cd $initPath ; bash"
+}
+
+help(){
+    printf "\nModo de uso:\n\t./ssh_connect.sh [OPCIONES]\n"
+    printf "\nOpciones:\n\t-r: Path de inicio de la conección. Por defecto es ~/server/.\n"
+    printf "\t-d: Dirección ip a conectarse. Por defecto es 192.168.7.2.\n"
+    printf "\t-n: Nombre de usuario. Por defecto es \"ubuntu\".\n"
+    printf "\t-p: Password. Por defecto es \"temppwd\".\n"
+    printf "\t-h: Ayuda. Muestra este menu.\n\n"
+}
+
+#____________________ Program flow ____________________#
+
+### Parameters Parser ###
+while [ "$1" != "" ]; do
+    case $1 in
+        -d )    shift
+                    ipAddr=$1
+                    ;;
+        -n )    shift
+                    usrName=$1
+                    ;;
+	    -p )    shift
+                    pwd=$1
+                    ;;
+        -r )    shift
+                    initPath=$1
+                    ;;
+        -h )        help
+                    exit
+                    ;;
+        * )         help
+                    exit 1
+    esac
+    shift
+done
+
+if [ -z "$pwd" ] || [ -z "$ipAddr" ] || [ -z "$initPath" ] || [ -z "$usrName" ]
+then
+    help
+    exit
+fi
+
+### Connection ###
+connect
