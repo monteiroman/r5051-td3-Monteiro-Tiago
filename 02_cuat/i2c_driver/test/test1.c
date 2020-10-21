45bf4d6de5806a14a88b5afba0dfc82c1f5209ac
Tiago Monteiro <tmonteiro@est.frba.utn.edu.ar>
2020-10-20 22:23:07 -0300

    Calculo de grados para compas

--- 02_cuat/i2c_driver/test/test1.c
+++ 02_cuat/i2c_driver/test/test1.c
@@ -2,15 +2,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <math.h>
+
+#define PI 3.141592654
 
 void printValues (int *values){
     printf("Data: ----------------------\n");
     printf("Accel:\n");
-    printf("X: %+.5d\tY: %+.5d\tZ: %+.5d\n", (int16_t)values[0], (int16_t)values[1], 
-        (int16_t)values[2]);
+    printf("X: %+.5d\tY: %+.5d\tZ: %+.5d\n", (int16_t)values[0], 
+        (int16_t)values[1], (int16_t)values[2]);
     printf("Mag:\n");
-    printf("X: %+.3d\t\tY: %+.3d\t\tZ: %+.3d\n", (int16_t)values[3], (int16_t)values[4], 
-        (int16_t)values[5]);
+    printf("X: %+.3d\t\tY: %+.3d\t\tZ: %+.3d\n", (int16_t)values[3], 
+        (int16_t)values[4], (int16_t)values[5]);
+
+    // Calculate the angle of the vector y,x
+    float heading = (atan2((int16_t)values[4],(int16_t)values[3]) * 180) / PI;
+
+    // Normalize to 0-360
+    if (heading < 0){
+        heading = 360 + heading;
+    }
+
+    printf("\nSensor heading: %.2f°\n", heading);
+    
 }
 
 int main (){
